1622444877ee5fb40f0a7f9a4b2260d7a28ab734
allen.okech <user@crud-app-vm.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-03 01:45:48 +0000

    Can finally add songs to playlists

--- application/forms.py
+++ application/forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired
+from application.models import Playlist
 
 class SongForm(FlaskForm):
     song_title = StringField("Song Title", validators=[DataRequired()])
     artist_name = StringField("Artist's Name", validators=[DataRequired()])
     genre = StringField("Song Genre", validators=[DataRequired()])
-    release_year = StringField("Year of Release", validators=[DataRequired()])
+    release_year = IntegerField("Year of Release", validators=[DataRequired()])
+    playlist = SelectField("Select Playlist", choices=[(p.id, p.playlist_name) for p in Playlist.query.order_by("playlist_name")])
     submit = SubmitField("Add Song")
 
 class PlaylistForm(FlaskForm):
-    playlist_name = StringField("Name of the Playlist", validators=[DataRequired()])
+    playlist_name = StringField("Name of the Playlist", validators=[DataRequired()]) 
     submit = SubmitField("Create Playlist")
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from application.models import Song, Playlist, association_table
+from application.models import Song, Playlist
 from application.forms import SongForm, PlaylistForm
 from flask import render_template, request, redirect, url_for
 
@@ -10,10 +10,9 @@ def home():
     all_songs = Song.query.all()
     return render_template("index.html", title="Home", all_playlists=all_playlists, all_songs=all_songs)
 
-@app.route('/view/<int:id>')
-def view_playlist(id):
-    playlist = Playlist.query.all()
-    playlist = Playlist.query.get(id)
+@app.route('/view/<int:playlist_id>')
+def view_playlist(playlist_id):
+    playlist = Playlist.query.get(playlist_id)
 
     return render_template('view.html', playlist=playlist)
 
@@ -34,17 +33,17 @@ def addsong():
     form = SongForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            new_song = Song(song_title=form.song_title.data)
+            new_song = Song(song_title=form.song_title.data, artist_name=form.artist_name.data, genre=form.genre.data, release_year=form.release_year.data, playlist=form.playlist.data)
             db.session.add(new_song)
             db.session.commit()
             return redirect(url_for("home"))
     
-    return render_template("add.html", title="Add a Song", form=form)
-
-@app.route('/update/<int:id>', methods=['GET','POST'])
-def update(id):
+    return render_template('add.html', title='Add a Song', form=form)
+     
+@app.route('/update/<int:playlist_id>', methods=['GET','POST'])
+def update(playlist_id):
     form = PlaylistForm()
-    playlist = Playlist.query.filter_by(id=id).first()
+    playlist = Playlist.query.filter_by(id=playlist_id).first()
     if request.method == 'POST':
         playlist.playlist_name = form.playlist_name.data
         db.session.commit()
@@ -52,9 +51,16 @@ def update(id):
 
     return render_template("update.html", form=form, title="Updated Playlist", playlist=playlist)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    playlist = Playlist.query.filter_by(id=id).first()
+@app.route('/delete/<int:playlist_id>')
+def delete(playlist_id):
+    playlist = Playlist.query.filter_by(id=playlist_id).first()
     db.session.delete(playlist)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route('/deletesong/<int:song_id>')
+def deletesong(song_id):
+    song = Song.query.filter_by(id=song_id).first()
+    db.session.delete(song)
     db.session.commit()
     return redirect(url_for("home"))
