c8895239f392f7aaf3e84e92320b59481a106842
allen.okech <user@crud-app-vm.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-04 16:39:04 +0000

    Changes

--- jenkinsStrtUp.sh
+++ jenkinsStrtUp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
-sudo apt update
-sudo apt-get install python3-venv
+sudo apt-get update
+sudo apt-get install python3 -y
+sudo apt-get install python3-venv -y
+sudo apt-get install python3-pip -y
 
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-sudo mkdir /opt/Music-Playlist-App
-sudo chown -R jenkins /opt/Music-Playlist-App
+python3 app.py
 
 sudo systemctl daemon-reload
-sudo systemctl stop music-playlist.service
 sudo systemctl start music-playlist.service
