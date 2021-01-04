31495245e6edae20b6eef95466655252d9b8a23d
allen.okech <user@crud-app-vm.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-04 16:29:59 +0000

    Jenkins startup and automation files

--- jenkinsStrtUp.sh
+++ jenkinsStrtUp.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get install python3 -y
-sudo apt-get install python3-venv -y
-sudo apt-get install python3-pip -y
+#!/bin/bash 
+sudo apt update
+sudo apt-get install python3-venv
 
 python3 -m venv venv
-
 source venv/bin/activate
-pip install -r requirements.txt
-python3 app.py
+pip3 install -r requirements.txt
+
+sudo mkdir /opt/Music-Playlist-App
+sudo chown -R jenkins /opt/Music-Playlist-App
+
+sudo systemctl daemon-reload
+sudo systemctl stop music-playlist.service
+sudo systemctl start music-playlist.service
