a5953ccc274bbc48c30a96f10c4662b3e16b5ba3
allen.okech <user@crud-app-vm.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-04 17:58:04 +0000

    pytest changes again

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-pytest --cov=application tests/test_unit.py --cov-report term-missing --disable-warnings
+pytest --cov=application --cov-report term-missing --disable-warnings
 
 sudo systemctl daemon-reload
 sudo systemctl stop music-playlist.service
