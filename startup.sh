64fac8edd780587250f4ab69819f693b744bb54c
allen.okech <user@crud-app-vm.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-04 17:04:45 +0000

    Changes to startup again

--- startup.sh
+++ startup.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
-cd /opt/Music-Playlist-App
-sudo python3 -m venv venv
+#!/bin/bash 
+
+sudo apt update
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-venv
+
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 app.py
+
+tests/test_unit.py --cov=application --cov-report term-missing --disable warnings
+
+sudo mkdir /opt/Music-Playlist-App/application
+sudo chown -R jenkins /opt/Music-Playlist-App/application
+
+sudo systemctl daemon-reload
+sudo systemctl stop music-playlist.service
+sudo systemctl start music-playlist.service
